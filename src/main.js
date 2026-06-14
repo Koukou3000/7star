0a114c1b7a4e4a3eb0540ed860aa28f7b90d9ca1
Koukou3000 <82990830+Koukou3000@users.noreply.github.com>
2026-06-15 02:02:01 +0800

    Refactor and fix CombinePredict functionality  (#5)
    * chore: add vite.config.js
    
    * refactor: 全选按钮的状态由计算属性管理
    
    * fix: checkbox 的使用细节，不应在el-checkbox使用:check
    
    * fix: sharedRound获取时机，修复输入框有一瞬间为0的情况
    
    * v-if解决sharedround被多个组件监听的问题
    
    * 修改项目为VueRouter（避免加载无效组件）
    
    * 调整功能清单
    
    * chore: 调整需求清单
    
    * chore: 设计新功能
    
    * feat: History添加请求abort()减少多余请求
    
    * fix: 调整this.result数组拼接策略
    
    * chore: 调整功能清单实现顺序
    
    * chore: 调整功能清单顺序
    
    * 拦截器cancel冗余请求前存档
    
    * 拦截器切换页面时cancel所有之前的请求
    
    * fix: 切换页面时cancel多余请求【路由守卫+拦截器】
    
    * refactor: 统一管理各页面的tableName【引用单一数据源@/constants】
    
    * chore: 调整功能清单
    
    * refactor: CombinePredict提取方法公共部分
    
    * fix: 修复CombinePredict修改期号时数据异步竞态；checkbox从@change改用watch监听
    
    * fix: 初次进入页面时请求两遍的问题
    
    * fix: 参数相同的predict请求不发起新的网络连接，复用同一份数据
    
    * fix: 不同页面的请求不互相影响
    
    * feat: 从其他页面进入CombinePredict页时，不cancel请求而是复用
    
    * fix: 只允许同一页面中复用请求
    
    * fix: 拆分api/index.js；activated()补充sharedRound为空时的情况
    
    * fix: 使用Object.freeze过滤不需要监听变化的常量
    
    * feat: 将CombinePredict的勾选记录存储在本地
    
    * chore: README fixed
    
    * fix: 调整 elementUI 为按需引入

--- src/main.js
+++ src/main.js
@@ -3,11 +3,28 @@ import App from './views/Home.vue'
 import store from './store';  // vuex
 import router from './router'
 
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Tabs, TabPane,
+  Row, Col, Input, Button, Loading,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup} from 'element-ui'
+  
+
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Input);
+Vue.use(Button);
+Vue.use(Loading);
+Vue.use(Checkbox);
+Vue.use(CheckboxButton);
+Vue.use(CheckboxGroup);
 
 
-Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
