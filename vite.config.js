2220f7cdb367a407768ba508ea057b2faa11910e
koukou3000 <1968204044@qq.com>
2026-06-15 13:38:28 +0800

    fix: 调整 elementUI 为按需引入

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
+import Components from 'unplugin-vue-components/vite'
+import { ElementUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue2()],
+  plugins: [
+    vue2(),
+    Components({ resolvers: [ElementUiResolver()] })
+  ],
   resolve: {
     alias: {
       '@': '/src'   
